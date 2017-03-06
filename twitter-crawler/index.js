8023867b22222f3128ef27212a9c3ff125942a98
Salim Salmi <salmi0707@gmail.com>
2017-03-06 14:15:17 +0100

    keywords

--- twitter-crawler/index.js
+++ twitter-crawler/index.js
@@ -5,8 +5,82 @@ const fs = require("fs");
 const csvWriter = require('csv-write-stream')
 const writer = csvWriter({headers: ["id","text","geo1","geo2","source","user.id","lang","timestamp_ms"]})
 
+var keywords = ["VVD",
+"PvdA",
+"PVV",
+"SP",
+"CDA",
+"D66",
+"ChristenUnie",
+"GroenLinks",
+"SGP",
+"Partij voor de Dieren",
+"pvdd",
+"50PLUS",
+"Rutte",
+"Asscher",
+"Wilders",
+"Roemer",
+"Buma",
+"Pechtold",
+"Segers",
+"Klaver",
+"Staaij",
+"Thieme",
+"Krol",
+"Bestuur",
+"Cultuur",
+"Economie",
+"Financiën",
+"Huisvesting",
+"Internationaal",
+"Landbouw",
+"Migratie",
+"Natuur",
+"Onderwijs",
+"Openbare orde",
+"Recht",
+"Ruimte",
+"Sociaal",
+"Verkeer",
+"Werk",
+"Zorg",
+"Integratie",
+"Milieu",
+"Veiligheid",
+"Wetenschap",
+"infrastructuur",
+"zekerheid",
+"gezondheid",
+"recreatie",
+"verzekering",
+"premie",
+"stem",
+"Nederland",
+"verkiezingen",
+"Banen",
+"buitenland",
+"ouderen",
+"groen",
+"stroom",
+"geld",
+"kamer",
+"coalitie",
+"asiel",
+"democratie",
+"politiek",
+"pijling",
+"partij",
+"Kabinet",
+"minister",
+"referendum",
+"wet",
+"kiezen"]
+
 const params = {
-  locations: '4.73,52.29,4.98,52.42'
+  locations: '4.73,52.29,4.98,52.42',
+  track: keywords.join(","),
+  language: "nl"
 }
 
 const stream = client.stream('statuses/filter', params);
@@ -18,8 +92,7 @@ stream.on('data', function(event) {
     return;
   }
   console.log(event.id);
-  console.log(event.coordinates);
-  console.log(event.geo);
+  
   var today = new Date();
 
   var geo = event.geo || event.coordinates;
